347dbdd1c331616b8b23bfbf75464471b411af33
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-07-11 11:32:13 +0530

    Create Beautiful_Matrix.cpp

---
+++ CodeForces/Beautiful_Matrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int matrix[5][5];
+    int row = 0, col = 0;
+
+
+    for (int i = 0; i < 5; ++i){
+        for (int j = 0; j < 5; ++j){
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1){
+                row = i;
+                col = j;
+            }
+        }
+    }
+    
+    int moves = abs(row - 2) + abs(col - 2);
+
+    cout << moves << endl;
+    
+    return 0;
+}
