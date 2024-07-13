bbcbb3c0464a610a530521757c78c258f09fe833
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-07-13 11:41:55 +0530

    Create Young_Physicist.cpp

---
+++ CodeForces/Young_Physicist.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int x = 0, y = 0, z = 0;
+    for (int i = 0; i < n; ++i){
+        int xi, yi, zi;
+        cin >> xi >> yi >> zi;
+        x += xi;
+        y += yi;
+        z += zi;
+    }
+    
+    if (x == 0 && y == 0 && z == 0) 
+        cout << "YES" << endl;
+    else 
+        cout << "NO" << endl;
+
+    return 0;
+}
