06181f73352ba4eaec9049eb77e80f75ec843dc5
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-08-13 17:50:45 +0530

    Create a.cpp

---
+++ CP/a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <cstdio>
+
+int main(){
+    int a,b;
+    string c;
+
+    cin >> a >> b >> c ;
+    cout << a << b << c ;
+
+    int d,e;
+    scanf("%d %d" , &a,&b);
+    printf("%d %d\n", a, b);
+}
